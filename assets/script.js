f073d893d78a81c76fc05a30485e72c2514ea8c0
Erin Platvoet <erin.didomenico520@gmail.com>
2020-10-07 19:31:36 -0400

    reorganized the layout of the functions so they weren't all inside the initial function

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,62 @@
-$(document).ready(function () {
-  console.log("READY!");
-  let zip = $("#city");
+$(document).ready(function(){
+    console.log("READY!");
 
-  //USER ENTERS CITY AND PRESSES BUTTON:
-  $("#getInfo").click(function () {
-    zip = $("#city").val();
-    displayInformation();
-  });
+    let zip = $("#city");
 
-  function displayInformation() {
-    const apiKey = "&APPID=940bd19264df2f0bdcef88196b007f5f";
-    const queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + apiKey;
+    //USER ENTERS CITY AND PRESSES BUTTON:
+    $("#getInfo").click(function () {
+        zip = $("#city").val();
+        displayInformation();
+    });
 
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(queryURL);
-      console.log(response);
-
-      $(".location").html("<h3>Currently in " + response.name + "</h3><br>");
-      $(".weather").html(
-        "Current conditions: " +
-          response.weather[0].description +
-          " <img src='https://openweathermap.org/img/w/" +
-          response.weather[0].icon +
-          ".png'>"
-      );
-      $(".coordinates").text(
-        "Coordinates: Lat:" +
-          response.coord.lat +
-          ", Lon: " +
-          response.coord.lon
-      );
-
-      // PULLING LAT/LONG FROM WEATHER API TO THEN PULL INFO FROM SEECLICKFIX.COM
-      let lat = response.coord.lat;
-      let lon = response.coord.lon;
-      const queryTwo =
-        "https://seeclickfix.com/open311/v2/services.json?lat=" +
-        lat +
-        "&long=" +
-        lon;
-
-      $.ajax({
-        url: queryTwo,
-        method: "GET",
-      }).then(function (services) {
-        display(services);
-        console.log(services);
-      });
-
-      function display(services) {
+    function displayInformation() {
+        const apiKey = "&APPID=940bd19264df2f0bdcef88196b007f5f";
+        const queryURL =
+          "https://api.openweathermap.org/data/2.5/weather?zip=" + zip + apiKey;
+    
+        $.ajax({
+          url: queryURL,
+          method: "GET",
+        }).then(function (response) {
+          console.log(queryURL);
+          console.log(response);
+    
+          $(".location").html("<h3>Currently in " + response.name + "</h3><br>");
+          $(".weather").html(
+            "Current conditions: " +
+              response.weather[0].description +
+              " <img src='https://openweathermap.org/img/w/" +
+              response.weather[0].icon +
+              ".png'>"
+          );
+          $(".coordinates").text(
+            "Coordinates: Lat:" +
+              response.coord.lat +
+              ", Lon: " +
+              response.coord.lon
+          );
+    
+          // PULLING LAT/LONG FROM WEATHER API TO THEN PULL INFO FROM SEECLICKFIX.COM
+          let lat = response.coord.lat;
+          let lon = response.coord.lon;
+          const queryTwo =
+            "https://seeclickfix.com/open311/v2/services.json?lat=" +
+            lat +
+            "&long=" +
+            lon;
+    
+          $.ajax({
+            url: queryTwo,
+            method: "GET",
+          }).then(function (services) {
+            display(services);
+            console.log(services);
+          });
+        })
+    }
+    function display(services) {
         $("#infoView").empty();
-        const displayResults = [];
+        const displayResults = {};
 
         for (i = 0; i < services[i].length; i++) {
           displayResults.push(createNewDisplay(services[i]));
@@ -74,6 +76,4 @@ $(document).ready(function () {
         $(org).append(servName);
         return newCol;
       }
-    });
-  }
-});
+})
